53b1d0bf7ce669e5fe983c9ea9f68f294c941e9c
Jacob <48930298+jstef99@users.noreply.github.com>
2019-07-05 22:17:35 +0200

    update RSA.java, add RSAapp2.java

--- RSA.java
+++ RSA.java
@@ -1,17 +1,26 @@
 package rsaapp;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.math.BigInteger;
 import java.util.StringTokenizer;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /*
    
+   This class provides static methods, all of them used in RSA process, also 
+   TwoThreeTuple class definition to return more than one long value.
+   Note that this implementation is not perfect, the main task was to represent 
+   the RSA process, value types used here are mainly long, for safer use Big 
+   Integer should be used. The primes binary file joined to these source files 
+   consists of primes that are smaller than 15 mln, much smaller than those in 
+   a real cryptography. Also the algorithm used in chaning characters into number
+   - here used making a long value, which consists of ASCII numbers - 2 digits for 
+   each character, as 22 is substracted. 
+   Example:
+   aB C ---ASCII--->(97)(66)(32)(67) --(-22)-->(75)(44)(10)(45)
+   Similar reverse process used in decrypting.
 
    Helpful sources used:
    https://primes.utm.edu/lists/small/millions/ - sample list of primes
@@ -54,7 +63,7 @@ public class RSA {
         }
         return new TwoThreeTuple(num1,num2);
     }
-    //comment
+    //returns greatest common divisor of a and b
     public static long GCD(long a, long b){
         if(a>b){
             if(b==0){
@@ -68,6 +77,7 @@ public class RSA {
             else return GCD(a,b%a);
         }
     }
+    //computes number b such, that (a*b)%n=1
     public static long modInv(long a, long n)
         {
           long a0,n0,p0,p1,q,r,t;
@@ -89,6 +99,8 @@ public class RSA {
           }
           return p1;
         }
+    //gerenerates Keys using function generatePrimes, inidcations used as in quite
+    //every RSA description
     public static TwoThreeTuple generateKeys(){
         long n,d=0,e=0,EulerVal;
         long tmp=0;
@@ -111,10 +123,10 @@ public class RSA {
         long tmp=0;
         //changing message into numbers
         for(i = 0; i < msg.length()/4; i++){
-            tmp=(msg.charAt(4*i)-30)*(long)Math.pow(10, 0);
-            tmp+=(msg.charAt(4*i+1)-30)*(long)Math.pow(10, 2);
-            tmp+=(msg.charAt(4*i+2)-30)*(long)Math.pow(10, 4);
-            tmp+=(msg.charAt(4*i+3)-30)*(long)Math.pow(10, 6);
+            tmp=(msg.charAt(4*i)-22)*(long)Math.pow(10, 0);
+            tmp+=(msg.charAt(4*i+1)-22)*(long)Math.pow(10, 2);
+            tmp+=(msg.charAt(4*i+2)-22)*(long)Math.pow(10, 4);
+            tmp+=(msg.charAt(4*i+3)-22)*(long)Math.pow(10, 6);
             long enc4b = fastExpoMod(tmp,e,n);
             encMsg.append(enc4b);
             if((4*i+3)!=msg.length()-1)
@@ -122,7 +134,7 @@ public class RSA {
         }
         tmp=0;
         for(j = i*4, k=0; j < msg.length();j++,k++){
-            tmp+=(msg.charAt(j)-30)*(long)Math.pow(10, 2*k);
+            tmp+=(msg.charAt(j)-22)*(long)Math.pow(10, 2*k);
         }
         if(tmp!=0){
             long enc4b = fastExpoMod(tmp,e,n);
@@ -132,14 +144,17 @@ public class RSA {
     }
     //decrypting with private key (d,n)
     public static String decrypt(String encMsg, long d, long n){
-        StringBuilder msg = new StringBuilder();
+        StringBuilder msg = new StringBuilder("");
         StringTokenizer tokens = new StringTokenizer(encMsg,"|");
+        System.out.println(encMsg);
         while(tokens.hasMoreTokens()){
             long val = Long.parseLong(tokens.nextToken());
-            System.out.println(fastExpoMod(val,d,n));
-            String valDecr = Long.toString(fastExpoMod(val,d,n)); //not always valid - why?
+            String valDecr = Long.toString(fastExpoMod(val,d,n)); 
+            System.out.println(valDecr.length()); 
             for(int i = 0; i < valDecr.length()/2;i++){
-                msg.append(valDecr.substring(valDecr.length()-2*(i+1), valDecr.length()-2*i));
+                String sign = valDecr.substring(valDecr.length()-2*(i+1), valDecr.length()-2*i);
+                char c = (char)(Long.parseLong(sign)+22);
+                msg.append(c);
             }
         }
         return msg.toString();
---
+++ RSAapp2.java
@@ -0,0 +1,274 @@
+package rsaapp;
+
+import rsaapp.RSA.TwoThreeTuple;
+
+/**
+ *
+ * This file contains GUI for encrypting/decrypting text using the RSA, generated 
+ * using swing Form.
+ * 
+ * 
+ */
+public class RSAapp2 extends javax.swing.JFrame {
+
+    public RSAapp2() {
+        initComponents();
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        buttonGroup1 = new javax.swing.ButtonGroup();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTextArea2 = new javax.swing.JTextArea();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jRadioButton1 = new javax.swing.JRadioButton();
+        jRadioButton2 = new javax.swing.JRadioButton();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        jLabel11 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        jTextArea2.setColumns(20);
+        jTextArea2.setRows(5);
+        jScrollPane2.setViewportView(jTextArea2);
+
+        jLabel1.setText("Input");
+
+        jLabel2.setText("Output");
+
+        jRadioButton1.setText("Encryption");
+        jRadioButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioButton1ActionPerformed(evt);
+            }
+        });
+
+        jRadioButton2.setText("Decryption");
+        jRadioButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioButton2ActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setText("Public key (e,n)");
+
+        jLabel4.setText("Private key (d,n)");
+
+        jLabel6.setText("d:");
+
+        jLabel7.setText("e:");
+
+        jLabel8.setText("n:");
+
+        jButton1.setText("Proceed");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("Generate keys");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jLabel9.setText("0");
+
+        jLabel10.setText("0");
+
+        jLabel11.setText("0");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 251, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 84, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 84, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addComponent(jScrollPane2))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(27, 27, 27)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jRadioButton2)
+                                    .addComponent(jRadioButton1))
+                                .addGap(0, 0, Short.MAX_VALUE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, 112, Short.MAX_VALUE)
+                                    .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(jButton2)
+                                        .addGap(0, 0, Short.MAX_VALUE)))
+                                .addContainerGap())))
+                    .addGroup(layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel6)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jLabel9, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jLabel10, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jLabel11, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jRadioButton1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jRadioButton2)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel5)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel6)
+                    .addComponent(jLabel9))
+                .addGap(2, 2, 2)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel7)
+                    .addComponent(jLabel10))
+                .addGap(5, 5, 5)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel8)
+                    .addComponent(jLabel11))
+                .addGap(38, 38, 38)
+                .addComponent(jButton2)
+                .addGap(26, 26, 26)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(23, 23, 23))
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jLabel1)
+                .addGap(5, 5, 5)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(12, 12, 12))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        boolean encryption = false;
+        if(jRadioButton1.isSelected()) encryption=true;
+        else if(jRadioButton2.isSelected()){}
+        else{jTextArea1.setText("Please provide valid encryption / decryption choice"); return;}
+        if(jLabel9.getText().equals("0")||jLabel10.getText().equals("0")||jLabel11.getText().equals("0")){
+             jTextArea1.setText("Please generate keys first");
+             return;
+        }
+        String msg = jTextArea1.getText();
+        if(msg.equals("")&&encryption){
+            jTextArea1.setText("Please enter text to proceed");
+            return;
+        }
+        String afterMsg;
+        if(encryption){
+            afterMsg=RSA.encrypt(msg,Long.parseLong(jLabel10.getText()) , Long.parseLong(jLabel11.getText()));
+        }
+        else{
+            afterMsg=RSA.decrypt(msg,Long.parseLong(jLabel9.getText()) , Long.parseLong(jLabel11.getText()));
+        }
+        jTextArea2.setText(afterMsg);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        TwoThreeTuple keys = RSA.generateKeys();
+        jLabel9.setText(keys.getFirst().toString());
+        jLabel10.setText(keys.getSecond().toString());
+        jLabel11.setText(keys.getThird().toString());
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jRadioButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton2ActionPerformed
+        if(jRadioButton1.isSelected()){
+            jRadioButton1.setSelected(false);
+            jRadioButton2.setSelected(true);
+        }
+        else{
+            jRadioButton2.setSelected(true);
+        }
+    }//GEN-LAST:event_jRadioButton2ActionPerformed
+
+    private void jRadioButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton1ActionPerformed
+        if(jRadioButton2.isSelected()){
+            jRadioButton2.setSelected(false);
+            jRadioButton1.setSelected(true);
+        }
+        else{
+            jRadioButton1.setSelected(true);
+        }
+    }//GEN-LAST:event_jRadioButton1ActionPerformed
+
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new RSAapp2().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JRadioButton jRadioButton1;
+    private javax.swing.JRadioButton jRadioButton2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextArea jTextArea2;
+    // End of variables declaration//GEN-END:variables
+}
